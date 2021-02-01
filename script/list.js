6e114ddd122ece5dba01b6f3893e5200f9a4da35
manujames <manujames1994@gmail.com>
2021-02-01 20:57:50 +0530

    Initial Version

--- script/list.js
+++ script/list.js
@@ -1,20 +1,25 @@
 let box = document.getElementById('box');
+
 function buildBox(){
     let xhttp = new XMLHttpRequest();
-    let listJson;
+    
     xhttp.onreadystatechange = function(){
         if(this.readyState == 1 || this.readyState == 2 || this.readyState == 3 && this.status == 200){
             box.innerHTML = "<h3 class='text-center'>Loading...</h3>";
         }
         else if(this.readyState == 4 && this.status == 200){
-            listJson = JSON.parse(this.responseText);
-            let htmlContent = "<table>";
-            for (let i in listJson.items){
-                htmlContent += "<tr><td>";
-                // htmlContent += Number(i)+1;
-                // htmlContent += "</td><td>"
-                htmlContent += listJson.items[i].name;
-                htmlContent += "</td></tr>";
+            let items = JSON.parse(this.responseText).items;
+            let htmlContent = "<table class='table table-hover'>";
+            htmlContent += "<tr class='table-success'><th>Item Code</th><th>Name</th><th>Quantity</th><th>Unit</th><th>Department</th><th>Notes</th></tr>"
+            for (let i in items){
+                htmlContent += "<tr>"
+                htmlContent += `<td>${items[i].sno}</td>`;
+                htmlContent += `<td>${items[i].name}</td>`;
+                htmlContent += `<td>${items[i].qty}</td>`;
+                htmlContent += `<td>${items[i].unit}</td>`;
+                htmlContent += `<td>${items[i].department}</td>`;
+                htmlContent += `<td>${items[i].notes}</td>`;
+                htmlContent += "</tr>";
             }
             htmlContent += "</table>";
             box.innerHTML = htmlContent;
@@ -26,7 +31,9 @@ function buildBox(){
             box.innerHTML = "<h3 class='text-center'>Sorry! Something went wrong.</h3>";
         }
     };
+    
     xhttp.open("GET", "../json/list.json",true);
     xhttp.send();
 }
+
 window.addEventListener('load',buildBox);
